864fb02221a06bcbc882652a4df4be72042f1343
Sascha Gaspar <sascha.gaspar@sva.de>
2023-01-02 11:19:36 +0100

    Setup nodes and prepare for Ansible
    Create 3 nodes with 3 volumes each
    Generate Ansible inventory
    Generate host-ssh-script

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "hcloud_network_subnet" "rancher_mgmt_subnet" {
 
 # VMs
 resource "hcloud_server" "rancher_mgmt_nodes" {
-    count = 2
+    count = 3
     name = "rancher-mgmt-${count.index + 1}"
     image = "debian-11"
     server_type = "cx11"
@@ -34,4 +34,54 @@ resource "hcloud_server_network" "rancher_node_subnet_registration" {
     count = 2
     server_id = hcloud_server.rancher_mgmt_nodes[count.index].id
     subnet_id = hcloud_network_subnet.rancher_mgmt_subnet.id
+}
+
+# Volumes
+
+resource "hcloud_volume" "rancher_volume-1" {
+  count = 2
+  name      = "rancher-volume-${count.index + 1}-1"
+  size      = 50
+  server_id = hcloud_server.rancher_mgmt_nodes[count.index].id
+  automount = true
+  format    = "xfs"
+}
+
+resource "hcloud_volume" "rancher_volume-2" {
+  count = 2
+  name      = "rancher-volume-${count.index + 1}-2"
+  size      = 50
+  server_id = hcloud_server.rancher_mgmt_nodes[count.index].id
+  automount = true
+  format    = "xfs"
+}
+
+resource "hcloud_volume" "rancher_volume-3" {
+  count = 2
+  name      = "rancher-volume-${count.index + 1}-3"
+  size      = 50
+  server_id = hcloud_server.rancher_mgmt_nodes[count.index].id
+  automount = true
+  format    = "xfs"
+}
+
+resource "local_file" "hosts_cfg" {
+    content = templatefile(
+        "${path.module}/inventory.tftpl",
+        {
+            user = "root"
+            nodes = hcloud_server.rancher_mgmt_nodes.*.ipv4_address
+        }
+    )
+    filename = "./inventory.yml"
+}
+
+resource "local_file" "setup_sh" {
+    content = templatefile(
+        "${path.module}/setup.tftpl",
+        {
+            nodes = hcloud_server.rancher_mgmt_nodes.*.ipv4_address
+        }
+    )
+    filename = "./setup.sh"
 }
